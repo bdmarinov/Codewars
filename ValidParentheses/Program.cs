44f06f0794d25058ef7475df53a9f18101a35d62
bdmarinov <bdmarinov@gmail.com>
2020-11-22 22:01:30 +0200

    Add files via upload

---
+++ ValidParentheses/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ValidParentheses
+{
+    class Program
+    {
+        public static bool ValidParentheses(string input)
+        {
+            if (input.Length >= 1)
+            {
+                Stack<char> s = new Stack<char>();
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (input[i] == '(')
+                    {
+                        s.Push('(');
+                    }
+                    if (input[i] == ')')
+                    {
+                        if (s.Count == 0)
+                        {
+                            return false;
+                        }
+                        s.Pop();
+                    }
+                }
+                if (s.Count == 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ValidParentheses("()")); //Should print true
+            Console.WriteLine(ValidParentheses(")(()))")); //Should print false
+            Console.WriteLine(ValidParentheses("(")); //Should print false
+            Console.WriteLine(ValidParentheses("(())((()())())")); //Should print true
+            Console.WriteLine(ValidParentheses("((()()))")); //Should print true
+        }
+    }
+}
