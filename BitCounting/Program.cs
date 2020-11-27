59e44858d6bfb68fd753ed1fb7a13a4ccadc78e7
Bogdan Marinov <bdmarinov@gmail.com>
2020-11-27 22:49:07 +0200

    Add files via upload

---
+++ BitCounting/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace BitCounting
+{
+    class Program
+    {
+        public static int CountBits(int n)
+        {
+            int count = 0;
+            while (n > 0)
+            {
+                if (n % 2 == 1)
+                {
+                    count++;
+                }
+                n /= 2;
+            }
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            Assert.AreEqual(0, CountBits(0));
+            Assert.AreEqual(1, CountBits(4));
+            Assert.AreEqual(3, CountBits(7));
+            Assert.AreEqual(2, CountBits(9));
+            Assert.AreEqual(2, CountBits(10));
+        }
+    }
+}
